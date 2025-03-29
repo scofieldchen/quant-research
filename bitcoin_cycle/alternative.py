9a1b74c9422303a4d8c2906132c8ca3e4557857f
scofieldchen <scofieldchen@163.com>
2025-03-29 09:31:19 +0800

    获取恐慌贪婪指数

---
+++ bitcoin_cycle/alternative.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+
+
+def get_fear_greed_index(limit: int = 10, timeout: int = 5) -> pd.DataFrame:
+    resp = requests.get(
+        "https://api.alternative.me/fng/", params={"limit": limit}, timeout=timeout
+    )
+    resp.raise_for_status()
+    data = resp.json()
+
+    return (
+        pd.DataFrame.from_records(data["data"], exclude=["time_until_update"])
+        .astype({"value": "float64", "timestamp": "int"})
+        .assign(datetime=lambda x: pd.to_datetime(x.timestamp, unit="s"))
+        .sort_values("datetime", ascending=True)
+        .set_index("datetime")
+        .drop(columns=["timestamp"])
+        .rename(columns={"value_classification": "sentiment"})
+    )
+
+
+if __name__ == "__main__":
+    df = get_fear_greed_index(10000)
+    print(df.tail())
+    print(df.info())
