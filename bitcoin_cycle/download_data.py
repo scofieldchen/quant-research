a8f9470a1778e83327ce4368d364368b2a12caa8
scofieldchen <scofieldchen@163.com>
2025-03-31 17:02:05 +0800

    下载区块链指标

--- bitcoin_cycle/download_data.py
+++ bitcoin_cycle/download_data.py
@@ -1,8 +1,39 @@
+import datetime as dt
 from pathlib import Path
 
-from rich import print
+import yfinance as yf
+from rich.console import Console
 
 from bgeometrics import BGClient
+
+console = Console()
+
+
+def download_ohlcv(data_directory: Path) -> None:
+    """从雅虎财经下载历史价格"""
+    ticker = "BTC-USD"
+    start_date = dt.datetime(2010, 1, 1)
+    end_date = dt.datetime.today()
+
+    console.print("\n====== Download ohlcv ======", style="bold blue")
+
+    data = yf.download(
+        ticker,
+        start=start_date,
+        end=end_date,
+        ignore_tz=True,
+        progress=False,
+        auto_adjust=True,
+    )
+    data.columns = data.columns.droplevel(1)  # 将多重索引转化为简单索引
+    data = data[["Open", "High", "Low", "Close", "Volume"]]
+    data.index.name = "datetime"
+    data.columns = [x.lower() for x in data.columns]
+
+    filepath = data_directory / f"{ticker.replace("-", "").lower()}.csv"
+    data.to_csv(filepath, index=True)
+
+    console.print(f"✅ Downloaded {ticker}", style="green bold")
 
 
 def download_blockchain_metrics(data_directory: Path) -> None:
@@ -19,12 +50,14 @@ def download_blockchain_metrics(data_directory: Path) -> None:
 
     client = BGClient()
 
+    console.print("\n====== Download blockchain metrics ======", style="bold blue")
+
     for metric in metrics:
         try:
             df = client.get_metric(metric)
-            print(f"Download metric {metric}: done", style="green bold")
+            console.print(f"✅ Downloaded {metric}", style="green bold")
         except Exception as e:
-            print(f"Failed to fet {metric}: {str(e)}", style="red bold")
+            console.print(f"❌ Failed to fet {metric}: {str(e)}", style="red bold")
         else:
             filepath = data_directory / f"{metric}.csv"
             df.to_csv(filepath, index=True)
@@ -38,6 +71,7 @@ def main() -> None:
     data_directory.mkdir(parents=True, exist_ok=True)
 
     # 下载数据
+    download_ohlcv(data_directory)
     download_blockchain_metrics(data_directory)
 
 
