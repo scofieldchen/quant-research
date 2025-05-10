339dc1eda345024df9c71c8870672e228a05920d
scofieldchen <scofieldchen@163.com>
2025-05-10 16:56:25 +0800

    更新数据管理

--- bitcoin_cycle/download_data.py
+++ bitcoin_cycle/download_data.py
@@ -1,41 +1,38 @@
-import datetime as dt
 from pathlib import Path
 
 import yfinance as yf
 from rich.console import Console
 
 from bgeometrics import BGClient
+from alternative import get_fear_greed_index
 
 console = Console()
+
+# 若无法从雅虎财经下载数据，设置本地 vpn 作为网络代理
+yf.set_config(proxy={"http": "http://127.0.0.1:7890", "https": "http://127.0.0.1:7890"})
 
 
 def download_ohlcv(data_directory: Path) -> None:
     """从雅虎财经下载历史价格"""
-    ticker = "BTC-USD"
-    start_date = dt.datetime(2010, 1, 1)
-    end_date = dt.datetime.today()
+    console.print("\n====== Price info ======", style="bold blue")
 
-    console.print("\n====== Download ohlcv ======", style="bold blue")
-
-    data = yf.download(
-        ticker,
-        start=start_date,
-        end=end_date,
-        ignore_tz=True,
-        progress=False,
-        auto_adjust=True,
-    )
-    data.columns = data.columns.droplevel(1)  # 将多重索引转化为简单索引
-    data = data[["Open", "High", "Low", "Close", "Volume"]]
-    data.index.name = "datetime"
-    data.columns = [x.lower() for x in data.columns]
-
-    filepath = data_directory / f"{ticker.replace("-", "").lower()}.csv"
-    data.to_csv(filepath, index=True)
-
-    console.print(
-        f"✅ Downloaded {ticker}, last:{data.index.max():%Y-%m-%d}", style="green bold"
-    )
+    try:
+        data = yf.download(
+            tickers="BTC-USD",
+            ignore_tz=True,
+            auto_adjust=True,
+            progress=False,
+            multi_level_index=False,
+        )
+        data = data[["Open", "High", "Low", "Close", "Volume"]]
+        data.index.name = "datetime"
+        data.columns = [x.lower() for x in data.columns]
+        console.print(f"✅ Downloaded BTCUSD, last:{data.index.max():%Y-%m-%d}")
+    except Exception as e:
+        console.print(f"❌ Failed to get btcusd ohlcv: {str(e)}")
+    else:
+        filepath = data_directory / "btcusd.csv"
+        data.to_csv(filepath, index=True)
 
 
 def download_blockchain_metrics(data_directory: Path) -> None:
@@ -50,20 +47,33 @@ def download_blockchain_metrics(data_directory: Path) -> None:
 
     client = BGClient()
 
-    console.print("\n====== Download blockchain metrics ======", style="bold blue")
+    console.print("\n====== Blockchain metrics ======", style="bold blue")
 
     for metric in metrics:
         try:
             df = client.get_metric(metric)
-            console.print(
-                f"✅ Downloaded {metric}, last:{df.index.max():%Y-%m-%d}",
-                style="green bold",
-            )
+            console.print(f"✅ Downloaded {metric}, last:{df.index.max():%Y-%m-%d}")
         except Exception as e:
-            console.print(f"❌ Failed to fet {metric}: {str(e)}", style="red bold")
+            console.print(f"❌ Failed to fet {metric}: {str(e)}")
         else:
             filepath = data_directory / f"{metric}.csv"
             df.to_csv(filepath, index=True)
+
+
+def download_fgi(data_directory: Path) -> None:
+    """下载恐慌和贪婪指数"""
+    console.print("\n====== Sentiment metrics ======", style="bold blue")
+
+    try:
+        df = get_fear_greed_index(limit=10 * 365)
+        console.print(
+            f"✅ Downloaded fear and greed index, last:{df.index.max():%Y-%m-%d}"
+        )
+    except Exception as e:
+        console.print(f"❌ Failed to get fear greed index: {str(e)}")
+    else:
+        filepath = data_directory / "fear_greed_index.csv"
+        df.to_csv(filepath, index=True)
 
 
 def main() -> None:
@@ -76,6 +86,7 @@ def main() -> None:
     # 下载数据
     download_ohlcv(data_directory)
     download_blockchain_metrics(data_directory)
+    download_fgi(data_directory)
 
 
 if __name__ == "__main__":
